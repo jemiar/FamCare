7e7ddd3a8ceb9d55e48930db8b6c314d68dbecc1
HoangMinh45 <hhuynh20@uic.edu>
2018-08-29 18:00:32 -0500

    Add fragments and swipe gesture

---
+++ app/src/main/java/com/project/hoangminh/famcare/Home.java
@@ -0,0 +1,90 @@
+package com.project.hoangminh.famcare;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity {
+
+    BottomNavigationView nav;
+    ViewPager pager;
+    MenuItem menuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        nav = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        pager = (ViewPager) findViewById(R.id.pager);
+
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.vital:
+                        pager.setCurrentItem(0);
+                        break;
+
+                    case R.id.mental:
+                        pager.setCurrentItem(1);
+                        break;
+
+                    case R.id.msg:
+                        pager.setCurrentItem(2);
+                        break;
+
+                    case R.id.people:
+                        pager.setCurrentItem(3);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if(menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    nav.getMenu().getItem(0).setChecked(false);
+                }
+                nav.getMenu().getItem(i).setChecked(true);
+                menuItem = nav.getMenu().getItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        setUpViewPager(pager);
+    }
+
+    private void setUpViewPager(ViewPager vp) {
+        ViewPagerAdapter vpa = new ViewPagerAdapter(getSupportFragmentManager());
+
+        Fragment vitalFrag = new VitalFragment();
+        Fragment mentalFrag = new MentalFragment();
+        Fragment msgFrag = new MessageFragment();
+        Fragment pplFrag = new PeopleFragment();
+
+        vpa.addFragment(vitalFrag);
+        vpa.addFragment(mentalFrag);
+        vpa.addFragment(msgFrag);
+        vpa.addFragment(pplFrag);
+
+        vp.setAdapter(vpa);
+    }
+}
--- app/src/main/java/com/project/hoangminh/famcare/Login.java
+++ app/src/main/java/com/project/hoangminh/famcare/Login.java
@@ -1,7 +1,10 @@
 package com.project.hoangminh.famcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login_Button = (Button) findViewById(R.id.button_login);
+
+        login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
